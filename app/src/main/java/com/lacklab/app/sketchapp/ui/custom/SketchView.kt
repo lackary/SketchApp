553b9c87c08ab8519ca74bee777eddc935d4a19c
lackary <lackary@gmail.com>
2021-12-15 22:49:22 +0800

    create some method about drawing

---
+++ app/src/main/java/com/lacklab/app/sketchapp/ui/custom/SketchView.kt
@@ -0,0 +1,148 @@
+package com.lacklab.app.sketchapp.ui.custom
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.lacklab.app.sketchapp.R
+import timber.log.Timber
+
+class SketchView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
+) : View(context, attrs, defStyle), SurfaceHolder.Callback {
+    private lateinit var latestPaintPen: Paint
+    private lateinit var latestPath: Path
+    private var paintPenList = ArrayList<Paint>()
+    private var pathList = ArrayList<Path>()
+
+    companion object {
+        const val STATE_STILL = 0;
+        const val STATE_MOVE = 1;
+        var DEFAULT_COLOR = 0;
+    }
+
+    init {
+//        holder.addCallback(this)
+    }
+
+    private var lineWidth = 15F
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        Timber.d("onAttachedToWindow")
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        Timber.d("onMeasure")
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        Timber.d("onLayout")
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+//        Timber.d("onDraw")
+        drawPath(canvas)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        val x = event?.x
+        val y = event?.y
+        Timber.d("onTouchEvent x: $x, y: $y")
+        when(event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                startPath(x!!, y!!)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                updatePath(x!!, y!!)
+//                val canvas = holder.lockCanvas()
+//                drawPath(canvas)
+//                holder.unlockCanvasAndPost(canvas)
+                invalidate()
+            }
+            MotionEvent.ACTION_UP ->{
+
+            }
+        }
+
+        return true
+    }
+
+    private fun initPaintPen() : Paint {
+        val paint = Paint()
+        with(paint) {
+            strokeWidth = lineWidth
+            isAntiAlias = true
+            isDither = true
+            style = Paint.Style.STROKE
+            strokeJoin = Paint.Join.MITER
+            strokeCap = Paint.Cap.ROUND
+            color = ContextCompat.getColor(context, R.color.white)
+        }
+        return paint
+    }
+
+    private fun initPath() = Path()
+
+    private fun initPen() {
+        latestPaintPen =  initPaintPen()
+        latestPath = initPath()
+        paintPenList.add(latestPaintPen)
+        pathList.add(latestPath)
+    }
+
+    private fun startPath(x:Float, y:Float) {
+        initPen()
+        latestPath.moveTo(x, y)
+    }
+
+    private fun updatePath(x:Float, y:Float) {
+        latestPath.lineTo(x, y)
+    }
+
+    private fun endPath(x:Float, y:Float) {
+
+    }
+
+    private fun drawPath(canvas: Canvas?) {
+        for(i in 0 until paintPenList.size) {
+            canvas?.apply {
+                drawPath(pathList[i], paintPenList[i])
+            }
+        }
+    }
+
+    private fun cleanDraw() {
+
+    }
+
+    private fun undoDraw() {
+
+    }
+
+    private fun resetPen(){
+
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        Timber.d("surfaceCreated")
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        Timber.d("surfaceChanged")
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        Timber.d("surfaceDestroyed")
+    }
+}
--- app/src/main/java/com/lacklab/app/sketchapp/ui/view/sketch/SketchFragment.kt
+++ app/src/main/java/com/lacklab/app/sketchapp/ui/view/sketch/SketchFragment.kt
@@ -1,19 +1,23 @@
 package com.lacklab.app.sketchapp.ui.view.sketch
 
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.lacklab.app.sketchapp.R
 import com.lacklab.app.sketchapp.base.BaseFragment
 import com.lacklab.app.sketchapp.databinding.FragmentSketchBinding
-import com.lacklab.app.sketchapp.ui.custom.SketchImageView
+import com.lacklab.app.sketchapp.ui.custom.SketchView
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
 @AndroidEntryPoint
-class SketchFragment : BaseFragment<FragmentSketchBinding, SketchViewModel>() {
+class SketchFragment :
+    BaseFragment<FragmentSketchBinding, SketchViewModel>(){
 
     private val sketchViewModel: SketchViewModel by viewModels()
 
-    private lateinit var sketchImageView: SketchImageView
+    private lateinit var sketchView: SketchView
 
     override val layoutId: Int
         get() = R.layout.fragment_sketch
@@ -22,12 +26,34 @@ class SketchFragment : BaseFragment<FragmentSketchBinding, SketchViewModel>() {
 
     override fun bindVM(binding: FragmentSketchBinding, viewModel: SketchViewModel) {
         with(binding) {
-            sketchImageView = sketchImageViewPaint
+            sketchView = sketchViewPaint
+//            sketchImageView.setOnTouchListener { v, event ->
+//                Timber.d("onTouch")
+//                when(event.action) {
+//                    MotionEvent.ACTION_DOWN -> {
+//                    }
+//                    MotionEvent.ACTION_MOVE -> {
+//                    }
+//                    MotionEvent.ACTION_UP ->{}
+//                }
+//                false
+//            }
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.d("onCreate")
     }
 
     override fun onResume() {
         super.onResume()
         Timber.d("onResume")
+//        sketchView.setOnTouchListener(this)
     }
+
+//    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+//        Timber.d("onTouch")
+//        return false
+//    }
 }
